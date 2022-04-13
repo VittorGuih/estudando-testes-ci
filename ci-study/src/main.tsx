d0db7d0d3a22cd6a8d24837f31b01968a63a836f
VittorGuih <vittytuuguilherme@gmail.com>
2022-04-13 14:36:53 -0300

    feat(ci-study): add a simple react project and lighthouse dependency.

---
+++ ci-study/src/main.tsx
@@ -0,0 +1,12 @@
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom';
+const App = lazy(() => import('./App'));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
